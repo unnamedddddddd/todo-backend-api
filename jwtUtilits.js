d9422a624c99fbd41bd8e7a0b88bb33eb5275946
Denis <deniskamaldinov85@gmail.com>
2026-01-07 23:18:15 +0500

    debug

--- jwtUtilits.js
+++ jwtUtilits.js
@@ -10,76 +10,79 @@ export const comparePassword = async (password, hashedPassword) => {
   return await bcrypt.compare(password, hashedPassword);
 }
 
-  export const generateToken = userId => {
-    return jwt.sign(
-      {userId},
-      process.env.JWT_SECRET,
-      {expiresIn: process.env.JWT_EXPIRES_IN}
-    )
+export const generateToken = userId => {
+  return jwt.sign(
+    {userId},
+    process.env.JWT_SECRET,
+    {expiresIn: process.env.JWT_EXPIRES_IN}
+  )
+}
+
+export const generateTokenRemember = userId => {
+  return jwt.sign(
+    {userId},
+    process.env.JWT_SECRET,
+    {expiresIn: process.env.JWT_REMEMBER_TOKEN}
+  )
+}
+
+export const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({
+        success: false,
+        message: 'Требуется авторизация. Токен не представлен'
+      });
   }
 
-  export const generateTokenRemember = userId => {
-    return jwt.sign(
-      {userId},
-      process.env.JWT_SECRET,
-      {expiresIn: process.env.JWT_REMEMBER_TOKEN}
-    )
-  }
-
-  export const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({
-          success: false,
-          message: 'Требуется авторизация. Токен не представлен'
-        });
-    }
-
-    const token = authHeader.split(' ')[1];
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = decoded.userId;
-      next();
-    } catch (error) {
-      if (error.name === 'TokenExpiredError') {
-        return res.status(401).json({
-          success: false, 
-          message: 'Токен истек. Войдите заново'
-        });
-      }
-      if (error.name === 'JsonWebTokenError') {
-        return res.status(401).json({
-          success: false, 
-          message: 'Неверный токен авторизации'
-        });
-      }
-      
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
       return res.status(401).json({
         success: false, 
-        message: 'Ошибка авторизации'
+        message: 'Токен истек. Войдите заново'
       });
     }
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({
+        success: false, 
+        message: 'Неверный токен авторизации'
+      });
+    }
+    
+    return res.status(401).json({
+      success: false, 
+      message: 'Ошибка авторизации'
+    });
   }
+}
 
-  export const rememberMiddleware  = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
-    
-    if (!token) {
-      return res.status(401).json({ 
-        success: false, 
-        message: 'Требуется refresh token' 
-      });
-    }
-    
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET_REMEMBER);
-      req.userId = decoded.userId;
-      next();
-    } catch (error) {
-      return res.status(401).json({ 
-        success: false, 
-        message: 'Неверный или истёкший refresh token' 
-      });
-    }
+export const rememberMiddleware  = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (!token) {
+    return res.status(401).json({ 
+      success: false, 
+      message: 'Требуется refresh token' 
+    });
   }
+  
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_REMEMBER);
+    console.log(decoded)
+    console.log(token)
+    console.log(process.env.JWT_SECRET_REMEMBER)
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ 
+      success: false, 
+      message: 'Неверный или истёкший refresh token' 
+    });
+  }
+}
 
